4435f1809b0055f89e61db5f658803d4cf1faf2f
bryelandt <brieuc.ryelandt@gmail.com>
2020-03-01 13:18:23 +0100

    fridays_1-2am_selection

--- assessment.R
+++ assessment.R
@@ -21,10 +21,38 @@ UTC <- ymd_hm(UTC) # convert strings to dates
 CET <- UTC + dhours(1)
 
 fridays <- CET[which(wday(CET) == 6)]
+fridays_pulses <- pulses[which(wday(CET) == 6)]
 fridays_list <- unique(floor_date(fridays, unit = "day"))
+
+# first visualization ----
 
 onedaytime <- CET[which(floor_date(CET, unit = "day") == fridays_list[1])]
 onedaypulses <- pulses[which(floor_date(CET, unit = "day") == fridays_list[1])]
 
 plot(onedaytime,onedaypulses,'h')
 title(fridays_list[1])
+# sélection vdd 1-2am ----
+
+fridays_night <- fridays[which(hour(fridays) >= 1 & hour(fridays) < 2)]
+fridays_night_pulses <- fridays_pulses[which(hour(fridays) >= 1 & hour(fridays) < 2)]
+
+plot(fridays_night,fridays_night_pulses,'h')
+
+onedaytime <- fridays_night[which(floor_date(fridays_night, unit = "day") == fridays_list[10])]
+onedaypulses <- fridays_night_pulses[which(floor_date(fridays_night, unit = "day") == fridays_list[10])]
+
+plot(onedaytime,onedaypulses,'h')
+title(fridays_list[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
